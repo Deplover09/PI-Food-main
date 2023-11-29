11377d465d72bdb2b40120628d8d6af456f82395
Santiago Acuña <santfloo7xcrl8@gmail.com>
2023-11-29 03:38:40 -0300

    dist updated

--- api/dist/index.js
+++ api/dist/index.js
@@ -5,7 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./server"));
 const database_1 = __importDefault(require("./database"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const { MONGODB_URI } = process.env;
 server_1.default.listen(server_1.default.get("port"), () => {
+    if (typeof MONGODB_URI !== "string") {
+        throw new Error("Please define the DB_URL environment variable inside .env.local");
+    }
     (0, database_1.default)()
         .then(() => {
         console.log("Database is connected.");
@@ -13,5 +19,6 @@ server_1.default.listen(server_1.default.get("port"), () => {
     })
         .catch((error) => {
         console.error("Error connecting to the database:", error);
+        throw new Error(error);
     });
 });
